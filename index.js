295c8853d979b3658420f0f8d479ea61b3e054e2
lasrysha <lasrysha@post.bgu.ac.il>
2023-05-18 13:18:18 +0300

    add

--- index.js
+++ index.js
@@ -134,6 +134,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.static('build'))
+
 app.use(express.json())
 
 const cors = require('cors')
@@ -220,7 +222,7 @@ let notes = [
   
 
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PASSENGER_PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
